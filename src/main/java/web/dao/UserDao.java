be857e4ce35c247f1dfdefd80d30224b7c3241b0
Вадим <Vadyok95@mail.ru>
2022-04-17 21:21:24 +0500

    111

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,18 @@
+
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+
+    void delete(Long id);
+
+    void update(User user);
+
+    List<User> getList();
+
+    User getUser(Long id);
+}
